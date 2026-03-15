9448754e02ffa14b78823b1cbf77cb6a6e297a00
fattan <khalifafattan05@gmail.com>
2026-03-16 02:10:55 +0700

    menambahkan class KodePos dan DoorMachine

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+        Console.WriteLine("Kode Pos Batununggal: " + kp.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Kujangsari: " + kp.GetKodePos("Kujangsari"));
+
+        Console.WriteLine();
+
+        DoorMachine pintu = new DoorMachine();
+        pintu.BukaPintu();
+        pintu.KunciPintu();
+    }
+}
